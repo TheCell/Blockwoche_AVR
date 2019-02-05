783fb50afc9c5055af6a7a90f92ecc4a6f67e0ff
maryneechan <yukichan13@web.de>
2019-02-05 19:37:47 +0100

    Spawner Change

---
+++ steamvrTest/Assets/FolderMarina/SpawnerMarina.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerMarina : MonoBehaviour {
+
+        public GameObject[] cubes;
+        public Transform[] points;
+        public float beat = (60 / 130);
+        public float timer;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (timer > beat)
+            {
+            int index = 80;
+
+
+                GameObject cube = Instantiate(cubes[Random.Range(0, cubes.Length)], points[Random.Range(0, points.Length)]);
+            // programmieren über 
+
+                cube.transform.localPosition = Vector3.zero;
+                cube.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
+                timer -= beat;
+            }
+
+            timer += Time.deltaTime;
+        }
+}
+
