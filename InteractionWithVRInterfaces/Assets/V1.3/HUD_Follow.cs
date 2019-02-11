e7a64f779e4be8a3cb4cc0dc193c8f7f7acae6bd
Kevin Huber <kevin.huber@hotmail.com>
2019-02-11 10:56:14 +0100

    First version HUD

---
+++ InteractionWithVRInterfaces/Assets/V1.3/HUD_Follow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD_Follow : MonoBehaviour
+{
+    [SerializeField] private Transform panel;
+    [SerializeField] private float threshhold;
+
+    private Transform cam;
+    private Vector3 distance;
+
+    private void Start()
+    {
+        cam = GetComponent<Transform>();
+        distance = panel.position - cam.position;
+    }
+
+    private void LateUpdate()
+    {
+        panel.LookAt(cam);
+        panel.position = cam.position + distance;
+    }
+}
