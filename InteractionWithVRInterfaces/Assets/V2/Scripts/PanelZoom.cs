3211b4323aec9c7b164f42c6acc1c8b57e2878ca
TheCell <simon_hischier@hotmail.com>
2019-02-12 12:27:41 +0100

    controllpanel Template

---
+++ InteractionWithVRInterfaces/Assets/V2/Scripts/PanelZoom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelZoom : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void zoomIn()
+	{
+		Vector3 scale = gameObject.transform.localScale;
+		scale.x += 0.3f;
+		scale.y += 0.3f;
+		scale.z += 0.3f;
+		gameObject.transform.localScale = scale;
+	}
+
+	public void zoomOut()
+	{
+		Vector3 scale = gameObject.transform.localScale;
+		scale.x -= 0.3f;
+		scale.y -= 0.3f;
+		scale.z -= 0.3f;
+		gameObject.transform.localScale = scale;
+	}
+
+	public void close()
+	{
+
+	}
+}
