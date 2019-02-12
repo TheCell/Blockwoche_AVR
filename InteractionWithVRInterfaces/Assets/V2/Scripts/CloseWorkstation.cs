780630bedcfd28df9a444a7856834b2fab143388
dertiger <danielschumi@bluewin.ch>
2019-02-12 14:51:24 +0100

    Deatached UI delayed close detached Ui try fix clos into not active window

--- InteractionWithVRInterfaces/Assets/V2/Scripts/CloseWorkstation.cs
+++ InteractionWithVRInterfaces/Assets/V2/Scripts/CloseWorkstation.cs
@@ -1,10 +1,14 @@
-﻿using Leap.Unity.Interaction;
+﻿using Leap.Unity.Animation;
+using Leap.Unity.Interaction;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CloseWorkstation : MonoBehaviour {
+    [SerializeField] private TransformTweenBehaviour transformTweenBehaviour;
+    [SerializeField] private Anchor anchor;
     private AnchorableBehaviour anchorableBehaviour;
+
 
     private void Start()
     {
@@ -13,15 +17,31 @@ public class CloseWorkstation : MonoBehaviour {
 
     public void AttachToParrentIfPossible()
     {
+        float waitTime = transformTweenBehaviour.tweenDuration;
+        transformTweenBehaviour.PlayBackward();
+        StartCoroutine(lateAttach(waitTime));
+    }
 
-        
-
+    IEnumerator lateAttach(float time)
+    {
+        yield return new WaitForSeconds(time);
+        bool wasVisible = anchor.gameObject.activeSelf;
+        if (!wasVisible)
+        {
+            anchor.gameObject.SetActive(true);
+        }
         Debug.Log("Try Attach - Result:");
         Anchor pref = anchorableBehaviour.GetNearestValidAnchor(false);
+        if (pref != null)
+        {
+            transform.position = pref.transform.position;
 
-        Debug.Log(pref);
-        transform.position = pref.transform.position;
-        bool result = anchorableBehaviour.TryAttachToNearestAnchor();
-        Debug.Log(result);
+            bool result = anchorableBehaviour.TryAttachToNearestAnchor();
+            Debug.Log(result);
+        }
+        if (!wasVisible)
+        {
+            anchor.gameObject.SetActive(false);
+        }
     }
 }
