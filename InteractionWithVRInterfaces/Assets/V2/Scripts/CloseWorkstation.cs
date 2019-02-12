4b8e9b13308462aa7d9bfb64d2c7d961e429fa03
dertiger <danielschumi@bluewin.ch>
2019-02-12 13:07:19 +0100

    try close Button

---
+++ InteractionWithVRInterfaces/Assets/V2/Scripts/CloseWorkstation.cs
@@ -0,0 +1,13 @@
+﻿using Leap.Unity.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseWorkstation : MonoBehaviour {
+    [SerializeField] private AnchorableBehaviour anchorableBehaviour;
+
+    public void AttachToParrentIfPossible()
+    {
+        anchorableBehaviour.TryAttachToNearestAnchor();
+    }
+}
