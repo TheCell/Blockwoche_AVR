d40c0bd924c54ff9e68f350f3363e6d85044ca60
Kevin Huber <kevin.huber@hotmail.com>
2019-02-13 14:41:57 +0100

    First version of the hud settings.

---
+++ InteractionWithVRInterfaces/Assets/V2/Scripts/HUDSettingsLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HUDSettingsLogic : MonoBehaviour
+{
+    [SerializeField] private GameObject hud;
+    [SerializeField] private TextMeshPro showHUD;
+
+    private void Start()
+    {
+    }
+
+    public void ToggleHud()
+    {
+        var substring = GetSubstring(showHUD.text);
+
+        if (hud.activeSelf)
+        {
+            hud.SetActive(false);
+            showHUD.text = "Show " + substring;
+        }
+        else
+        {
+            hud.SetActive(true);
+            showHUD.text = "Hide " + substring;
+        }
+    }
+
+    private string GetSubstring(string s)
+    {
+        return s.Substring(s.IndexOf(' '));
+    }
+}
