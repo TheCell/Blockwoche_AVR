e642895d7814d81196a86c3ed763f9fb52d738f0
Kevin Huber <kevin.huber@hotmail.com>
2019-02-13 13:24:57 +0100

    Added panel popup delay

--- InteractionWithVRInterfaces/Assets/V2/Scripts/MachineDisplayCheck.cs
+++ InteractionWithVRInterfaces/Assets/V2/Scripts/MachineDisplayCheck.cs
@@ -1,115 +1,109 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class MachineDisplayCheck : MonoBehaviour
 {
-	[SerializeField]
-	private GameObject[] machinePanels;
-	[SerializeField]
-	private GameObject noMachinesForPanelsSymbol;
-	[SerializeField]
-	private Material interactableMaterial;
-	[SerializeField]
-	private Material interactableActiveMaterial;
+    [SerializeField] private GameObject[] machinePanels;
+    [SerializeField] private GameObject noMachinesForPanelsSymbol;
+    [SerializeField] private Material interactableMaterial;
+    [SerializeField] private Material interactableActiveMaterial;
 
-	private GameObject[] allInteractibleGameObjects;
+    private GameObject[] allInteractibleGameObjects;
+    private float timer;
 
-	// Use this for initialization
-	void Start ()
-	{
-		hideAllPanels();
+    // Use this for initialization
+    private void Start()
+    {
+        HideAllPanels();
         allInteractibleGameObjects = GameObject.FindGameObjectsWithTag("interactibleObject");
         HideAllInteractibles();
     }
 
-    // Update is called once per frame
-    void Update ()
-	{
-		
-	}
-
-	public void DisplayAllInteractibles()
-	{
-		for (int i = 0; i < allInteractibleGameObjects.Length; i++)
-		{
+    public void DisplayAllInteractibles()
+    {
+        for (int i = 0; i < allInteractibleGameObjects.Length; i++)
+        {
             MeshRenderer objectRenderer = allInteractibleGameObjects[i].GetComponent<MeshRenderer>();
             objectRenderer.enabled = true;
 
             var interactibleRenderer = allInteractibleGameObjects[i].GetComponent<Renderer>();
-			if (interactibleRenderer != null)
-			{
-				interactibleRenderer.material = this.interactableMaterial;
-			}
-		}
-	}
-
-	public void HideAllInteractibles()
-	{
-		for (int i = 0; i < allInteractibleGameObjects.Length; i++)
-		{
-            MeshRenderer objectRenderer = allInteractibleGameObjects[i].GetComponent<MeshRenderer>();
-            objectRenderer.enabled = false;
-		}
-	}
-
-    private void OnTriggerStay(Collider other)
-    {
-        MachineID machineNumber = other.GetComponent<MachineID>();
-
-        if (machineNumber != null)
-        {
-            hideAllPanels();
-            int machineID = machineNumber.getMachineID();
-            //Debug.Log(machineID);
-            displayPanel(machineID);
-        }
-        else
-        {
-            hideAllPanels();
-            noMachinesForPanelsSymbol.SetActive(true);
-            //Debug.Log("Collider " + other.gameObject.name + " has no MachineID Script");
+            if (interactibleRenderer != null)
+            {
+                interactibleRenderer.material = this.interactableMaterial;
+            }
         }
     }
 
+    public void HideAllInteractibles()
+    {
+        for (int i = 0; i < allInteractibleGameObjects.Length; i++)
+        {
+            MeshRenderer objectRenderer = allInteractibleGameObjects[i].GetComponent<MeshRenderer>();
+            objectRenderer.enabled = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (timer > 0.5f)
+        {
+            MachineID machineNumber = other.GetComponent<MachineID>();
+
+            if (machineNumber != null)
+            {
+                HideAllPanels();
+                int machineID = machineNumber.getMachineID();
+                //Debug.Log(machineID);
+                DisplayPanel(machineID);
+            }
+            else
+            {
+                HideAllPanels();
+                noMachinesForPanelsSymbol.SetActive(true);
+            }
+
+            timer = 0f;
+        }
+
+        timer += Time.deltaTime;
+    }
+
     private void OnTriggerEnter(Collider other)
-	{
+    {
         Renderer boxRenderer = other.GetComponent<Renderer>();
 
         if (boxRenderer != null && other.gameObject.CompareTag("interactibleObject"))
-		{
-			boxRenderer.material = this.interactableActiveMaterial;
-		}
-	}
+        {
+            boxRenderer.material = this.interactableActiveMaterial;
+        }
+    }
 
-	private void OnTriggerExit(Collider other)
-	{
+    private void OnTriggerExit(Collider other)
+    {
         Renderer boxRenderer = other.GetComponent<Renderer>();
 
         if (boxRenderer != null && other.gameObject.CompareTag("interactibleObject"))
-		{
-			boxRenderer.material = this.interactableMaterial;
-		}
-	}
+        {
+            boxRenderer.material = this.interactableMaterial;
+        }
+    }
 
-	private void displayPanel(int panelID)
-	{
-		if (machinePanels[panelID] != null)
-		{
-			machinePanels[panelID].SetActive(true);
-		}
-		else
-		{
-			//Debug.Log("Machine with ID " + panelID + " does not exist");
-		}
-	}
+    private void DisplayPanel(int panelID)
+    {
+        if (machinePanels[panelID] != null)
+        {
+            machinePanels[panelID].SetActive(true);
+        }
+    }
 
-	private void hideAllPanels()
-	{
-		for (int i = 0; i < machinePanels.Length; i++)
-		{
-			machinePanels[i].SetActive(false);
-			noMachinesForPanelsSymbol.SetActive(false);
-		}
-	}
+    private void HideAllPanels()
+    {
+        for (int i = 0; i < machinePanels.Length; i++)
+        {
+            machinePanels[i].SetActive(false);
+            noMachinesForPanelsSymbol.SetActive(false);
+        }
+    }
 }
