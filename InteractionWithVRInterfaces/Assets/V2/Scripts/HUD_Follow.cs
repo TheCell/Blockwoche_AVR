9e7d6ea7c9613a37ab8569156996c72156997ec0
Kevin Huber <kevin.huber@hotmail.com>
2019-02-13 15:11:31 +0100

    HUD changes

--- InteractionWithVRInterfaces/Assets/V2/Scripts/HUD_Follow.cs
+++ InteractionWithVRInterfaces/Assets/V2/Scripts/HUD_Follow.cs
@@ -7,7 +7,8 @@ public class HUD_Follow : MonoBehaviour
     [SerializeField] private Transform panel;
     [SerializeField] private float threshhold;
     [SerializeField] private float distance;
-    [SerializeField] private float height;
+    [SerializeField] private float heightOffset;
+    [SerializeField] private float widthOffset;
 
     private Transform cam;
 
@@ -22,9 +23,10 @@ public class HUD_Follow : MonoBehaviour
         var camForward = cam.forward;
         camForward.y = 0f;
         panel.position = cam.position + camForward.normalized * distance;
-        var panelHeight = cam.localPosition.y - height;
+        var panelHeight = cam.localPosition.y - heightOffset;
         var position = panel.position;
         position.y = panelHeight > 0.2f ? panelHeight : 0.2f;
+        position.x += widthOffset;
         panel.position = position;
     }
 }
